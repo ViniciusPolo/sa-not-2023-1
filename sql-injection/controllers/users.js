be637a1ffd9ac7a93cb5fdcedefa904a3dc1f5d7
Vinicius Polo <86489843+ViniciusPolo@users.noreply.github.com>
2023-03-03 23:56:15 +0000

    03-03 primeira invasão

---
+++ sql-injection/controllers/users.js
@@ -0,0 +1,32 @@
+const conn = require('../config/database')
+
+const controller = {}
+
+controller.login = async (req,res) => {
+    try {
+       const sql = `
+        select * from users where username = '${req.body.username}'and password = '${req.body.password}'
+       ` 
+       console.log('SQL', sql)
+
+       const result = await conn.query(sql)
+
+       if (result){
+        res.render('loggedin',{
+            title: 'Seja Bem Vindo',
+            message: 'Usuário autenticado com sucesso',
+        })
+       }else {
+        res.render('loggedin',{
+            title: 'Acesso Negado',
+            message: 'Usuário o senha incorretos',
+        })
+       }
+
+    } catch (error) {
+       console.error(error)
+        
+    }
+};
+
+module.exports = controller
--- sql-injection/routes/users.js
+++ sql-injection/routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
+const controller = require('../controllers/users');
 var router = express.Router();
+
+router.post('/login', controller.login )
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
